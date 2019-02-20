8334dff6cde20418ea02cb2ca7039061bb313c4b
James Paterson <james@mjsquare.com>
2019-02-20 15:02:14 +0000

    add squad 1.1 data

--- train.py
+++ train.py
@@ -1,1 +1,10 @@
 # This file trains the neural network using the encoder and decoder.
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+i = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(i)
