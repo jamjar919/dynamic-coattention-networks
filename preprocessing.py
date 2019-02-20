a5cdb0b5aaf5777389118e46ef0e07d94990e248
James Paterson <james@mjsquare.com>
2019-02-20 16:05:02 +0000

    add glove

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+# preprocessing of the data
+
+import tensorflow as tf
+import pandas as pd 
+
+GLOVE_DATA_FILE = 'data/glove.840B.300d.txt'
+words = pd.read_table(GLOVE_DATA_FILE, sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
+
+def vec(w):
+  return words.loc[w].as_matrix()
+
+def text_to_vector(text):
+    tokens = tf.keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
+    for tok in tokens: 
+        return vec(tok)
+    return tokens;
--- train.py
+++ train.py
@@ -3,6 +3,38 @@
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import json
+import random
+
+# custom imports
+from preprocessing import text_to_vector
+
+# open the training file 
+TRAINING_FILE_NAME = 'data/dev.json'
+
+with open(TRAINING_FILE_NAME, "r") as f:
+    data = json.loads(f.read())
+    assert data["version"] == "1.1"
+    categories = data["data"]
+
+questions = [
+    {"question": "something", "answers":["something","else"]}
+];
+
+print("Loaded test data. Categories:")
+for category in categories:
+    print(category["title"] + ": " + str(len(category["paragraphs"])), end=", ")
+    for paragraph in category["paragraphs"]:
+        paragraph["context"] = text_to_vector(paragraph["context"])
+        for qas in paragraph["qas"]:
+            questions.append({
+                "context": paragraph["context"],
+                "question": text_to_vector(qas["question"]),
+                "answer": random.choice(qas["answers"])["text"]
+            })      
+    
+print(questions)
+
 
 i = tf.global_variables_initializer()
 
