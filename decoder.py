a3ed2ffeb16e03fd0ccd328bab2d07a3e4dc1038
Vidyaranya Sai <vidyaranya.ns@gmail.com>
2019-03-05 18:20:23 +0000

    decoder draft

--- decoder.py
+++ decoder.py
@@ -1,3 +1,16 @@
 # decode the question using the dynamic decoder
-def decoder(knowledge_rep,hidden_units = 200):
+import tensorflow as tf
+import random
+
+HIDDEN_STATE_SIZE = 200
+EMBEDDING_SIZE_OF_WORDS = 400
+DOCUMENT_SIZE = 632
+def decoder(encoder_states, batch_size, s_init, e_init):
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(HIDDEN_STATE_SIZE)
+    state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+
+    for i in range(4):
+        
+
+
     pass;
