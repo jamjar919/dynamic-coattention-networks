d7b5313229ead12122f436f47af8c09a052463de
milton19 <antonis19@mail.com>
2019-03-06 18:04:18 +0000

    update

--- highway_network.py
+++ highway_network.py
@@ -7,70 +7,66 @@ POOL_SIZE = 16
 def transpose(tensor):
     return tf.transpose(tensor,perm=[0,2,1])
 
-def highway_network_batch(U, lstm_hidden_state,
+def highway_network(U, lstm_hidden_state,
                     coattention_encoding_of_prev_start_word,
                     coattention_encoding_of_prev_end_word,
                     wd, w1, w2, w3,
                     b1, b2, b3):
 
-    U_transpose = transpose(U)
-    fn = lambda x : highway_network_matrix(x[0], x[1],
-                    x[2],
-                    x[3],
+    U_transpose = tf.transpose(U,perm=[1, 2, 0])
+    print("u_t shape:", U_transpose.shape)
+    fn = lambda batch_of_word_encodings : highway_network_batch(batch_of_word_encodings, lstm_hidden_state,
+                    coattention_encoding_of_prev_start_word,
+                    coattention_encoding_of_prev_end_word,
                     wd, w1, w2, w3,
                     b1, b2, b3)
     # returns 10 * 1
-    return tf.map_fn(fn, (U_transpose, lstm_hidden_state, 
-    coattention_encoding_of_prev_start_word, 
-    coattention_encoding_of_prev_end_word) )
+    return tf.map_fn(fn, U_transpose)
 
 # U_transpose is of size 632 * 400
-def highway_network_matrix(U_transpose, lstm_hidden_state,
-                    coattention_encoding_of_prev_start_word,
-                    coattention_encoding_of_prev_end_word,
-                    wd, w1, w2, w3,
-                    b1, b2, b3):
+# def highway_network_matrix(U_transpose, lstm_hidden_state,
+#                     coattention_encoding_of_prev_start_word,
+#                     coattention_encoding_of_prev_end_word,
+#                     wd, w1, w2, w3,
+#                     b1, b2, b3):
+#
+#     #U_transpose = tf.transpose(U, perm=[1, 0])
+#     fn = lambda col : highway_network_single(col, lstm_hidden_state,
+#                     coattention_encoding_of_prev_start_word,
+#                     coattention_encoding_of_prev_end_word,
+#                     wd, w1, w2, w3,
+#                     b1, b2, b3)
+#     # returns 1 number
+#     return tf.reduce_max(tf.map_fn(fn, U_transpose))
 
-    #U_transpose = tf.transpose(U, perm=[1, 0])
-    fn = lambda col : highway_network_single(col, lstm_hidden_state,
-                    coattention_encoding_of_prev_start_word,
-                    coattention_encoding_of_prev_end_word,
-                    wd, w1, w2, w3,
-                    b1, b2, b3)
-    # returns 1 number
-    return tf.reduce_max(tf.map_fn(fn, U_transpose))
-
-def highway_network_single(coattention_encoding_of_word_in_doc,
+def highway_network_batch(batch_of_word_encodings,
                     lstm_hidden_state,
                     coattention_encoding_of_prev_start_word,
                     coattention_encoding_of_prev_end_word,
                     wd, w1, w2, w3,
                     b1, b2, b3):
-
-    print("coattention_encoding_of_word shape: ",coattention_encoding_of_word_in_doc.shape)
-    print("lstm_hidden_state shape: ",lstm_hidden_state.shape)
-    print("coattention_encoding_of_prev_start_word shape: ",coattention_encoding_of_prev_start_word.shape)
-    print("coattention_encoding_of_prev_end_word shape: ",coattention_encoding_of_prev_end_word.shape)
     # weight_initer = tf.truncated_normal_initializer(mean=0.0, stddev=0.01)
     #wd = tf.get_variable("wd", shape=[HIDDEN_STATE_SIZE, 5 * HIDDEN_STATE_SIZE],
     #                     initializer=weight_initer)
 
-    # calculate r. The dimension of r would be L * 1
+    # calculate r. The dimension of r would be l * 1
+    batch_size = batch_of_word_encodings.shape[0]
+    print("batch_size : ",batch_size)
     con = tf.concat(values=[lstm_hidden_state, coattention_encoding_of_prev_start_word,
-                               coattention_encoding_of_prev_end_word], axis=0)
-    print("CONCATENATION SUCCESSFUL con.shape : ",con.shape)
-    con_reshaped = tf.reshape(con, [ 1, con.shape[0], -1])
-    wd_reshaped = tf.reshape(wd, [ 1, wd.shape[0], wd.shape[1]])
-    linear_model = tf.matmul(wd_reshaped, con_reshaped)
-    print("linear_model shape ",linear_model.shape)
+                               coattention_encoding_of_prev_end_word], axis=1)
+    print("con.shape:", con.shape)
+    # wd = tf.tile(wd,[batch_size])
+    print("wd.shape: ",wd.shape)
+    linear_model = tf.map_fn(lambda x: tf.matmul(wd, x, transpose_a=True),con)
+    print("linear_model shape: ",linear_model.shape)
     activated_value = tf.nn.tanh(linear_model)
-    tf.reshape(activated_value, [1, HIDDEN_STATE_SIZE])
+    tf.reshape(activated_value, [HIDDEN_STATE_SIZE])
 
     # calculate mt1
     #w1 = tf.get_variable("w1", shape=[POOL_SIZE, HIDDEN_STATE_SIZE, 3 * HIDDEN_STATE_SIZE],
     #                     initializer=weight_initer)
     # b1 = tf.get_variable("b1", shape=[POOL_SIZE, HIDDEN_STATE_SIZE])
-    con2 = tf.concat(values=[coattention_encoding_of_word_in_doc, activated_value],axis=0)
+    con2 = tf.concat(values=[coattention_encoding_of_word_in_doc, activated_value])
     mt1_premax = tf.reshape(tf.matmul(w1, con2), [POOL_SIZE, HIDDEN_STATE_SIZE]) + b1
     mt1_postmax =  tf.reduce_max(mt1_premax, axis=0)
     mt1_postmax_reshaped = tf.reshape(mt1_postmax, [HIDDEN_STATE_SIZE])
@@ -87,12 +83,8 @@ def highway_network_single(coattention_encoding_of_word_in_doc,
     #w3 = tf.get_variable("w3", shape=[POOL_SIZE, HIDDEN_STATE_SIZE, 2 * HIDDEN_STATE_SIZE],
     #                     initializer=weight_initer)
     # b3 = tf.get_variable("b3", shape=[POOL_SIZE])
-    con3 = tf.concat(values=[mt1_postmax_reshaped, mt2_postmax_reshaped],axis = 0)
+    con3 = tf.concat(values=[mt1_postmax_reshaped, mt2_postmax_reshaped])
     hmn_premax = tf.reshape(tf.matmul(w3, con3), [POOL_SIZE]) + b3
     hmn_postmax = tf.reduce_max(hmn_premax, axis=0)
 
     return hmn_postmax
-
-
-
-
