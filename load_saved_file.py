f9c29031fcdd122db93a213aa2941a1b0206c4f1
James Paterson <james@mjsquare.com>
2019-03-13 18:44:23 +0000

    load saved file modifications

--- load_saved_file.py
+++ load_saved_file.py
@@ -4,6 +4,7 @@ import numpy as np
 import tensorflow as tf
 import sklearn as sk
 from functools import reduce
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 # custom imports
 from dataset import Dataset
@@ -16,19 +17,21 @@ print(index2embedding.shape)
 
 
 tf.reset_default_graph()
-imported_graph = tf.train.import_meta_graph('trained-dcnn.meta')
+imported_graph = tf.train.import_meta_graph('./model/saved.meta')
 
 init = tf.global_variables_initializer()
-batch_size = 10
+batch_size = 16
 
 with tf.Session() as sess:
 
-    imported_graph.restore(sess, 'trained-dcnn.meta')
-    question_batch_placeholder = imported_graph.get_tensor_by_name("question_batch:0")
-    context_batch_placeholder = imported_graph.get_tensor_by_name("context_batch:0")
-    answer_start_batch_predict = imported_graph.get_tensor_by_name("answer_start_batch:0")
-    answer_end_batch_predict = imported_graph.get_tensor_by_name("answer_end_batch:0")
-    #question: question_batch, context: context_batch, answer_start: answer_start_batch, answer_end: answer_end_batch
+    imported_graph.restore(sess, './model/saved')
+    graph = tf.get_default_graph()
+
+    question_batch_placeholder = graph.get_tensor_by_name("question_batch:0")
+    context_batch_placeholder = graph.get_tensor_by_name("context_batch:0")
+    answer_start_batch_predict = graph.get_tensor_by_name("answer_start:0")
+    answer_end_batch_predict = graph.get_tensor_by_name("answer_end:0")
+    embedding = graph.get_tensor_by_name("embedding:0")
 
     sess.run(init)
     print("SESSION INITIALIZED")
@@ -41,22 +44,33 @@ with tf.Session() as sess:
         answer_start_batch_actual = np.array(list(map(lambda qas: (qas["answer_start"]), batch))).reshape(batch_size)
         answer_end_batch_actual = np.array(list(map(lambda qas: (qas["answer_end"]), batch))).reshape(batch_size)
         print("BEFORE ENCODER RUN counter = ", counter)
-        loss_val = sess.run([answer_start_batch_predict, answer_end_batch_predict], feed_dict={
+        s, e = sess.run([answer_start_batch_predict, answer_end_batch_predict], feed_dict={
             question_batch_placeholder: question_batch,
             context_batch_placeholder: context_batch,
+            embedding: index2embedding
         })
 
+        print(answer_start_batch_actual, answer_end_batch_actual)
+
+        predictions = np.concatenate([s, e])
+        actual = np.concatenate([answer_start_batch_actual, answer_end_batch_actual])
+
+        print("predictions",predictions)
+        print("actual", actual)
 
         print(
         "Precision", sk.metrics.precision_score(
-                np.concatenate(answer_start_batch_predict.eval(), answer_end_batch_predict.eval()),
-                np.concatenate(answer_start_batch_actual.eval(), answer_end_batch_actual.eval())))
+                predictions,
+                actual)
+        )
         print(
         "Recall", sk.metrics.recall_score(
-                np.concatenate(answer_start_batch_predict.eval(), answer_end_batch_predict.eval()),
-                np.concatenate(answer_start_batch_actual.eval(), answer_end_batch_actual.eval())))
+                predictions,
+                actual)
+        )
         print(
         "f1_score", sk.metrics.f1_score(
-                np.concatenate(answer_start_batch_predict.eval(), answer_end_batch_predict.eval()),
-                np.concatenate(answer_start_batch_actual.eval(), answer_end_batch_actual.eval())))
+                predictions,
+                actual)
+        )
 
--- main.py
+++ main.py
@@ -11,29 +11,30 @@ padded_data, index2embedding, max_length_question, max_length_context = D.load_d
 print(padded_data[0])
 batch_size = 16
 embedding_dimension = 300
+init = tf.global_variables_initializer()
 
 saver = tf.train.import_meta_graph('./model/saved.meta')
 with tf.Session() as sess:
+    sess.run(init)
     saver.restore(sess, './model/saved')
 
     graph = tf.get_default_graph()
     s = graph.get_tensor_by_name("answer_start:0")
     e = graph.get_tensor_by_name("answer_end:0")
+    question_batch_placeholder = graph.get_tensor_by_name("question_batch:0")
+    context_batch_placeholder = graph.get_tensor_by_name("context_batch:0")
+    embedding = graph.get_tensor_by_name("embedding:0")
 
     question = np.array([padded_data[0]["question"]] * batch_size, dtype = np.int32)
     context = np.array([padded_data[0]["context"]] * batch_size, dtype = np.int32)
 
-    embedding = tf.placeholder(shape = [len(index2embedding), embedding_dimension], dtype=tf.float32, name='embedding')
-    question_batch = tf.placeholder(dtype=tf.int32, shape = [batch_size, max_length_question], name='question_batch')
-    context_batch = tf.placeholder(dtype=tf.int32, shape = [batch_size, max_length_context], name='context_batch')
-
     print(question.shape)
     print(context.shape)
 
-    result = sess.run([s, e], feed_dict = {
-        #question_batch : question,
-        context_batch : context,
-        #embedding: index2embedding
+    s_result, e_result = sess.run([s, e], feed_dict = {
+        question_batch_placeholder : question,
+        context_batch_placeholder : context,
+        embedding: index2embedding
     })
     
-    print(result)
+    print(s_result, e_result)
