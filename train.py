1db684df884cb46e9a5f6c0a27a0543b96865cc1
James Paterson <james@mjsquare.com>
2019-03-13 17:52:07 +0000

    saving and loading

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ padded_data, index2embedding, max_length_question, max_length_context = D.load_d
 print("Loaded data")
 
 # Train now
-batch_size = 64
+batch_size = 16
 embedding_dimension = 300
 tf.reset_default_graph()
 
@@ -73,17 +73,6 @@ with tf.Session() as sess:
         })
         print("loss: ",np.mean(loss_val))
         counter += batch_size%len(padded_data)
-    '''
-    tf.saved_model.simple_save(
-        sess,
-        '/model',
-        inputs = {
-            question_batch: question_batch_placeholder,
-            context_batch: context_batch_placeholder
-        },
-        outputs = {
-            answer_start: s,
-            answer_end: e
-        }
-    )
-    '''
+
+    saver.save(sess, './model/saved') 
+    
