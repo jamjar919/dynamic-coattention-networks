8632aeab1b8e9a569b9b5a1795ed7b80d80e7394
Panos A <anpa29@gmail.com>
2019-03-15 20:35:16 +0000

    Added sequence length to encoder, removed dropout, appending sentinel vector to the beginning before removing it.

--- encoder.py
+++ encoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 def length(sequence):
-    used = tf.sign(tf.reduce_max(tf.abs(sequence), axis=2))
+    used = tf.sign(tf.reduce_max(tf.abs(sequence), axis= 2))
     length = tf.reduce_sum(used, axis=1)
     length = tf.cast(length, tf.int32)
     return length
@@ -17,7 +17,7 @@ def encoder(questions,contexts,embedding,hidden_unit_size=200,embedding_vector_s
         contexts: Tensor of contexts
         embedding: Mappings from encoded questions to GLoVE vectors
     '''
-    dropout_rate = 0.3 # https://openreview.net/forum?id=rJeKjwvclx Authors claim to use 0.3 rate. 
+    #dropout_rate = 0.3 # https://openreview.net/forum?id=rJeKjwvclx Authors claim to use 0.3 rate. 
     batch_size = questions.shape[0]
     #contexts_size = contexts.shape[1].value
     questions_size = questions.shape[1].value
@@ -27,11 +27,11 @@ def encoder(questions,contexts,embedding,hidden_unit_size=200,embedding_vector_s
     assert contexts.shape[0] == batch_size, "context shape doesn't match batch size"
 
     # Vectorise the contexts and questions
-    # Format [batch, length, depth]
+    # Format [batch, length, depth]  
     context_vector = tf.map_fn(lambda x:  tf.nn.embedding_lookup(embedding, x), contexts, dtype=tf.float32)
-    question_vector = tf.map_fn(lambda x:  tf.nn.embedding_lookup(embedding, x), questions, dtype=tf.float32)
-    context_vector = tf.nn.dropout(context_vector, keep_prob = dropout_rate) # Add dropout
-    question_vector = tf.nn.dropout(question_vector, keep_prob = dropout_rate)
+    question_vector = tf.map_fn(lambda x:  tf.nn.embedding_lookup(embedding, x), questions, dtype=tf.float32) # shape 10x33x300
+    #context_vector = tf.nn.dropout(context_vector, keep_prob = dropout_rate) # Add dropout
+    #question_vector = tf.nn.dropout(question_vector, keep_prob = dropout_rate)
 
     context_embedding = tf.transpose(context_vector, perm=[0, 2, 1])
     question_embedding = tf.transpose(question_vector, perm=[0, 2, 1])
@@ -41,22 +41,24 @@ def encoder(questions,contexts,embedding,hidden_unit_size=200,embedding_vector_s
     lstm_enc = tf.nn.rnn_cell.LSTMCell(hidden_unit_size)
 
     # https://stackoverflow.com/questions/48238113/tensorflow-dynamic-rnn-state/48239320#48239320
-    context_encoding, _ = tf.nn.dynamic_rnn(lstm_enc, transpose(context_embedding), dtype=tf.float32)
-    context_encoding = transpose(context_encoding)
+    context_embedding_T = transpose(context_embedding) # c_e_T is shape 10x632x300 now.
+    context_encoding, _ = tf.nn.dynamic_rnn(lstm_enc, context_embedding_T, sequence_length = length(context_embedding_T, axis_dim = 2), dtype=tf.float32)
+    context_encoding = transpose(context_encoding) # This is now 10x200x632
 
-    # Append sentinel vector
+    # Append sentinel vector (to the beginning now)
     # https://stackoverflow.com/questions/52789457/how-to-perform-np-append-type-operation-on-tensors-in-tensorflow
     sentinel_vec_context = tf.Variable(tf.random_uniform([hidden_unit_size,1]), dtype = tf.float32)
     sentinel_vec_context_batch = tf.stack([sentinel_vec_context] * batch_size)
-    context_encoding = tf.concat([context_encoding,sentinel_vec_context_batch], axis  =-1)
+    context_encoding = tf.concat([sentinel_vec_context_batch, context_encoding], axis  =-1)
 
-    question_encoding, _ = tf.nn.dynamic_rnn(lstm_enc, transpose(question_embedding), dtype=tf.float32)
+    question_embedding_T = transpose(question_embedding)
+    question_encoding, _ = tf.nn.dynamic_rnn(lstm_enc, question_embedding_T, sequence_length = length(question_embedding_T, axis_dim = 2), dtype=tf.float32)
     question_encoding = transpose(question_encoding)
     
-    # Append sentinel vector
+    # Append sentinel vector (to the beginning)
     sentinel_vec_question = tf.Variable(tf.random_uniform([hidden_unit_size,1]), dtype = tf.float32)
-    sentinel_vec_q_batch = tf.stack([sentinel_vec_question] * batch_size)
-    question_encoding = tf.concat([question_encoding, sentinel_vec_q_batch], axis = -1)
+    sentinel_vec_q_batch = tf.stack([sentinel_vec_question] * batch_size) # shape 128,200,1
+    question_encoding = tf.concat([sentinel_vec_q_batch, question_encoding], axis = -1) # shape 128,200,41
 
     assert question_encoding.shape == (batch_size, hidden_unit_size, questions.shape[1] + 1), "question encoding shape doesn't match (batch size, hidden unit size, max question length + 1) " + str(question_encoding.shape)
     assert context_encoding.shape == (batch_size, hidden_unit_size, contexts.shape[1] + 1), "context encoding shape doesn't match (batch size, hidden unit size, max context length + 1) " + str(context_encoding.shape)
@@ -75,7 +77,7 @@ def encoder(questions,contexts,embedding,hidden_unit_size=200,embedding_vector_s
     assert L.shape == (batch_size, contexts.shape[1] + 1,  questions.shape[1] + 1), "L shape doesn't match (batch_size, max context length + 1,  max question length + 1)" + str(L.shape)
 
     # attention weights for questions A^{Q} = softmax(L)
-    A_q = tf.nn.softmax(L)
+    A_q = tf.nn.softmax(L) # rowwise on the rows of the matrices in the tensor. 
     assert A_q.shape == (batch_size, contexts.shape[1] + 1,  questions.shape[1] + 1), "A_q shape doesn't match (batch_size, max context length + 1,  max question length + 1)" + str(A_q.shape)
 
     # attention weights for documents A^{D} = softmax(L')
@@ -97,10 +99,14 @@ def encoder(questions,contexts,embedding,hidden_unit_size=200,embedding_vector_s
     # Bi-LSTM
     cell_fw = tf.nn.rnn_cell.LSTMCell(hidden_unit_size)  
     cell_bw = tf.nn.rnn_cell.LSTMCell(hidden_unit_size)
-    u_states, _ = tf.nn.bidirectional_dynamic_rnn(cell_bw=cell_bw,cell_fw=cell_fw,dtype=tf.float32,inputs= transpose(C))
+    C_transpose = transpose(C)
+    u_states, _ = tf.nn.bidirectional_dynamic_rnn(cell_bw=cell_bw,cell_fw=cell_fw,dtype=tf.float32,inputs = C_transpose, sequence_length = length(C_transpose))
     
     U = tf.concat(u_states, axis = 2) # 10x633x400
-    U = U[:,:-1,:] # Make U to 10x632x400
+    print("U SHAPE LINE 107: ", U.shape)
+    #U = U[:,:-1,:] 
+    U = tf.slice(U, begin = [0,1,0], size = [batch_size, contexts.shape[1], 2*hidden_unit_size]) # Make U to 10x632x400
+    print("U SHAPE AFTER SLICE:", U.shape)
     assert U.shape == (batch_size, contexts.shape[1], 2 * hidden_unit_size), "C shape doesn't match (batch_size, 2 * hidden_unit_size, max context length)" + str(U)
     
     return U 
