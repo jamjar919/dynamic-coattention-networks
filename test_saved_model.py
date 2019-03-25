99b8795b69af3eb43e3950173fab45b5102fa4de
Panos A <anpa29@gmail.com>
2019-03-25 18:34:35 +0000

    Added prints to testing

--- test_saved_model.py
+++ test_saved_model.py
@@ -2,9 +2,7 @@
 import sys
 import numpy as np
 import tensorflow as tf
-import sklearn as sk
 from functools import reduce
-from sklearn.metrics import precision_score, recall_score, f1_score
 
 # custom imports
 from dataset import Dataset
@@ -18,14 +16,14 @@ padded_data, index2embedding, max_length_question, max_length_context = D.load_d
 print("Loaded data")
 
 tf.reset_default_graph()
-latest_checkpoint_path = tf.train.latest_checkpoint('./model/')
+latest_checkpoint_path = './model/saved-23'
 print("restoring from "+latest_checkpoint_path)
 saver = tf.train.import_meta_graph(latest_checkpoint_path+'.meta')
 
-init = tf.global_variables_initializer()
+#init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
-    sess.run(init)
+    #sess.run(init)
     saver.restore(sess, latest_checkpoint_path)
     graph = tf.get_default_graph()
     answer_start_batch_predict = graph.get_tensor_by_name("answer_start:0")
@@ -34,11 +32,9 @@ with tf.Session() as sess:
     context_batch_placeholder = graph.get_tensor_by_name("context_batch_ph:0")
     embedding = graph.get_tensor_by_name("embedding_ph:0")
     dropout_keep_rate = graph.get_tensor_by_name("dropout_keep_ph:0")
+    loss  = graph.get_tensor_by_name("loss_to_optimize:0")
 
-    sess.run(init)
     f1score = []
-    precision = []
-    recall = []
 
     print("SESSION INITIALIZED")
     for iteration in range(0, len(padded_data) - CONFIG.BATCH_SIZE, CONFIG.BATCH_SIZE):
@@ -46,15 +42,16 @@ with tf.Session() as sess:
         batch = padded_data[iteration:(iteration + CONFIG.BATCH_SIZE)]
         question_batch, context_batch, answer_start_batch_actual, answer_end_batch_actual = get_batch(batch, CONFIG.BATCH_SIZE, max_length_question, max_length_context)
         
-        estimated_start_index, estimated_end_index = sess.run([answer_start_batch_predict, answer_end_batch_predict], feed_dict={
+        estimated_start_index, estimated_end_index, loss = sess.run([answer_start_batch_predict, answer_end_batch_predict, loss], feed_dict={
             question_batch_placeholder: question_batch,
             context_batch_placeholder: context_batch,
             embedding: index2embedding,
             dropout_keep_rate: 1
         })
-
-        predictions = np.concatenate([estimated_start_index, estimated_end_index])
-        actual = np.concatenate([answer_start_batch_actual, answer_end_batch_actual])
+        print("Loss: ", np.mean(loss))
+        print("estimated start index: ", estimated_start_index)
+        print("estimated end index: ", estimated_end_index)
+        print(estimated_start_index.shape)
 
         f1 = 0            
         for i in range(len(estimated_end_index)):
@@ -65,21 +62,13 @@ with tf.Session() as sess:
                 context_batch[i],
                 D
             )
-        f1score.append(f1/len(estimated_end_index))
+        f1score_curr = f1/len(estimated_end_index)
+        print("Current f1 score: ", f1score_curr)
+        f1score.append(f1score_curr)
 
-        precision.append(sk.metrics.precision_score(
-                predictions,
-                actual, average='micro'))
-        recall.append(sk.metrics.recall_score(
-                predictions,
-                actual, average='micro'))
+        #if(iteration % ((CONFIG.BATCH_SIZE)-1) == 0):
+        print("Tested (",iteration,"/",len(padded_data),")")
 
-        if(iteration % ((CONFIG.BATCH_SIZE)-1) == 0):
-            print("Tested (",iteration,"/",len(padded_data),")")
-
-
-    print("Precision mean: ", np.mean(precision))
-    print("Recall mean: ", np.mean(recall))
     print("F1 mean: ", np.mean(f1score))
 
 
