346a16ab08b4925b6c41855474abf14e7af756c8
milton19 <antonis19@mail.com>
2019-04-03 21:43:06 +0100

    added score with testing

--- score.py
+++ score.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Score :
 
     def __init__(self):
@@ -11,7 +13,7 @@ class Score :
         self.F1 = 0
 
     def update(self, predicted_labels, actual_labels):
-        for i in range(predicted_labels.shape[0]):
+        for i in range(len(predicted_labels)):
             if actual_labels[i] == 0 :
                 if predicted_labels[i] == 0 :
                     self.true_negatives+=1
@@ -23,12 +25,17 @@ class Score :
                 elif predicted_labels[i] == 1:
                     self.true_positives+=1
         self.update_stats()
+
+    def safe_div(self,n,d) :
+        if d == 0:
+            return 0
+        return n/d
     
     def update_stats(self) :
-        self.precision = self.true_positives / (self.true_positives + self.false_positives)
-        self.recall = self.true_positives / (self.true_positives+self.false_negatives)
-        self.F1 = 2 * (self.precision * self.recall) / (self.precision + self.recall)
-        self.accuracy = (self.true_positives + self.true_negatives) / (self.true_positives + 
+        self.precision = self.safe_div(self.true_positives,self.true_positives + self.false_positives)
+        self.recall = self.safe_div(self.true_positives, self.true_positives+self.false_negatives)
+        self.F1 = self.safe_div(2 * (self.precision + self.recall), (self.precision * self.recall))
+        self.accuracy = self.safe_div(self.true_positives + self.true_negatives , self.true_positives + 
             self.true_negatives + self.false_positives + self.false_negatives)
     
     def reset(self):
@@ -40,3 +47,23 @@ class Score :
         self.recall = 0 
         self.accuracy = 0
         self.F1 = 0
+
+    def print_stats(self):
+        print(f"tp: {self.true_positives}, tn: {self.true_negatives}, fp: {self.false_positives}, fn: {self.false_negatives}")
+        print(f"precision = {self.precision}, recall = {self.recall}, F1 = {self.F1}, accuracy = {self.accuracy}")
+
+if __name__ == '__main__':
+    score = Score()
+    score.print_stats()
+    score.update([1,1,1,1],[1,1,1,1])
+    score.print_stats()
+    # test with np array
+    score.update(np.array([0,0,0,0]),[0,0,0,0])
+    score.print_stats()
+    score.update([1,1,0,0],[0,0,1,1])
+    score.print_stats()
+    score.update([1],[0])
+    score.print_stats()
+    score.update([0],[1])
+    score.print_stats()
+
