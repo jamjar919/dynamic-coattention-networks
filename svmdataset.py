c1feb9fdfbfd396306dc253974ec4709f2421ab8
Panos A <anpa29@gmail.com>
2019-04-03 15:24:16 +0100

    SVM data for squad 2

--- svmdataset.py
+++ svmdataset.py
@@ -17,16 +17,16 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 D = Dataset(CONFIG.EMBEDDING_FILE)
 index2embedding = D.index2embedding
-padded_data_squad1, (max_length_question, max_length_context) = D.load_questions('data/train.json')
-padded_data_validation = padded_data_squad1[(int) (CONFIG.TRAIN_PERCENTAGE*len(padded_data_squad1)):]
-untrained_contexts = [x["context"] for x in padded_data_validation]
-print("Loaded data from squad one")
+#padded_data_squad1, (max_length_question, max_length_context) = D.load_questions('data/train.json')
+#padded_data_validation = padded_data_squad1[(int) (CONFIG.TRAIN_PERCENTAGE*len(padded_data_squad1)):]
+#untrained_contexts = [x["context"] for x in padded_data_validation]
+#print("Loaded data from squad one")
 
 padded_data_squad2, (max_length_question_squad2, max_length_context_squad2) = D.load_questions('data/train-v2.0.json')
 print("padded_data_squad2.len = ",len(padded_data_squad2))
 print("Max length from squad 2 q and c: ", max_length_question_squad2, max_length_context_squad2)
 print("Loaded data from squad two")
-
+'''
 padded_data_untrained = [x for x in padded_data_squad2 if x["context"] in untrained_contexts]
 unanswerable_data = [x for x in padded_data_untrained if x["answer_start"]==-1]
 answerable_data = [x for x in padded_data_untrained if x["answer_start"]>=0]
@@ -34,8 +34,12 @@ print("Number of unanswerable questions: ",len(unanswerable_data))
 print("Number of answerable questions: ", len(answerable_data))
 
 padded_data = np.array(padded_data_untrained)
+'''
+padded_data = np.array(padded_data_squad2)
+padded_data = padded_data[(int) ((CONFIG.TRAIN_PERCENTAGE)*len(padded_data_squad2)) : ]
+print(padded_data.shape)
 
-latest_checkpoint_path = './model/saved-7' 
+latest_checkpoint_path = './modelv2/saved-4' 
 print("restoring from "+latest_checkpoint_path)
 saver = tf.train.import_meta_graph(latest_checkpoint_path+'.meta')
 
@@ -57,31 +61,29 @@ with tf.Session(config=config) as sess:
     alpha_raw, beta_raw, labels = [], [], []
 
     for iteration in range(0, len(padded_data) - CONFIG.BATCH_SIZE, CONFIG.BATCH_SIZE):
-            # running on an example batch to debug encoder
-            batch = padded_data[iteration : (iteration + CONFIG.BATCH_SIZE)]
-            question_batch, context_batch, answer_start_batch_actual, answer_end_batch_actual = get_batch(batch, CONFIG.BATCH_SIZE, max_length_question, max_length_context)
+        # running on an example batch to debug encoder
+        batch = padded_data[iteration : (iteration + CONFIG.BATCH_SIZE)]
+        question_batch, context_batch, answer_start_batch_actual, answer_end_batch_actual = get_batch(batch, CONFIG.BATCH_SIZE, max_length_question, max_length_context)
+        print("Iteration: ", iteration , "out of ", len(padded_data))
+        alphas, betas = sess.run([alphas_predict, betas_predict], feed_dict={
+            question_batch_placeholder: question_batch,
+            context_batch_placeholder: context_batch,
+            embedding: index2embedding,
+            dropout_keep_rate: 1.0
+        })
+        
+        for i in range(answer_start_batch_actual.shape[0]):
+            alpha_raw.append((alphas[-1][i][:]))
+            beta_raw.append((betas[-1][i][:]))
+            
+            if answer_start_batch_actual[i] == -1:
+                labels.append(-1)
+            else:
+                labels.append(1)
 
-            alphas, betas = sess.run([alphas_predict, betas_predict], feed_dict={
-                question_batch_placeholder: question_batch,
-                context_batch_placeholder: context_batch,
-                embedding: index2embedding,
-                dropout_keep_rate: 1.0
-            })
-            #print(alphas.shape)
-            for i in range(answer_start_batch_actual.shape[0]):
-                alpha_raw.append(softmax(alphas[-1][i][:]))
-                beta_raw.append(softmax(betas[-1][i][:]))
-                
-                if answer_start_batch_actual[i] == -1:
-                    labels.append(-1)
-                else:
-                    labels.append(1)
-  
-            #print(len(alpha_raw))
-
-    with open('./SVMdata/alphas_raw.pkl', 'wb') as f:
+    with open('./SVMdata/alphas_rawv2.pkl', 'wb') as f:
         pickle.dump(alpha_raw, f, protocol=3)
-    with open('./SVMdata/betas_raw.pkl', 'wb') as f:
+    with open('./SVMdata/betas_rawv2.pkl', 'wb') as f:
         pickle.dump(beta_raw, f, protocol=3)
-    with open('./SVMdata/labels.pkl', 'wb') as f:
+    with open('./SVMdata/labelsv2.pkl', 'wb') as f:
         pickle.dump(labels, f, protocol = 3)
