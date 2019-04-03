21f206d52c6579ef80e6bdbb7f6e358ed285f59d
Panos A <anpa29@gmail.com>
2019-04-03 18:07:25 +0100

    some changes

--- network/classifier.py
+++ network/classifier.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from network.encoder import encoder
 import numpy as np
-from network.config import CONFIG
+from config import CONFIG
 
 def get_batch(batch, batch_size = CONFIG.BATCH_SIZE, max_length_question = CONFIG.MAX_QUESTION_LENGTH, max_length_context = CONFIG.MAX_CONTEXT_LENGTH):
     question_batch = np.array(list(map(lambda qas: (qas["question"]), batch))).reshape(batch_size, max_length_question)
@@ -9,18 +9,27 @@ def get_batch(batch, batch_size = CONFIG.BATCH_SIZE, max_length_question = CONFI
     has_answer_batch = np.array(list(map(lambda qas: (qas["has_answer"]), batch))).reshape(batch_size)
     return question_batch, context_batch, has_answer_batch
 
+def get_feed_dict(question_batch, context_batch, answer_batch, dropout_keep_prob, index2embedding) :
+    graph = tf.get_default_graph()
+    return {
+                graph.get_tensor_by_name("question_batch_ph:0") : question_batch,
+                graph.get_tensor_by_name("context_batch_ph:0") : context_batch,
+                graph.get_tensor_by_name("answer_start_true_ph:0"): answer_batch,
+                graph.get_tensor_by_name("dropout_keep_ph:0") : dropout_keep_prob,
+                graph.get_tensor_by_name("embedding_ph:0"): index2embedding
+            }
+
 def build_classifier(embedding):
     dropout_keep_rate = tf.placeholder(dtype = tf.float32, name = "dropout_keep_ph")
     batch_size_ph = tf.placeholder(dtype = tf.int32, name = "batch_size_ph")
-    question_batch_placeholder = tf.placeholder(dtype=tf.int32, shape = [CONFIG.BATCH_SIZE, CONFIG.MAX_QUESTION_LENGTH], name='question_batch_ph')
-    context_batch_placeholder = tf.placeholder(dtype=tf.int32, shape = [CONFIG.BATCH_SIZE, CONFIG.MAX_CONTEXT_LENGTH], name='context_batch_ph')
+    question_batch_ph = tf.placeholder(dtype=tf.int32, shape = [CONFIG.BATCH_SIZE, CONFIG.MAX_QUESTION_LENGTH], name='question_batch_ph')
+    context_batch_ph = tf.placeholder(dtype=tf.int32, shape = [CONFIG.BATCH_SIZE, CONFIG.MAX_CONTEXT_LENGTH], name='context_batch_ph')
     # Word index placeholders
-    answer_start = tf.placeholder(dtype=tf.int32,shape=[None], name='answer_start_true_ph')
-    answer_end = tf.placeholder(dtype=tf.int32,shape=[None], name='answer_end_true_ph')
+    has_answer_ph = tf.placeholder(dtype=tf.int32,shape=[None], name='has_answer_ph')
     max_context_length = CONFIG.MAX_CONTEXT_LENGTH
 
     # Create encoder. (Encoder will also return the sequence length of the context (i.e. how much of each batch element is unpadded))
-    U, context_seq_length = encoder(question_batch_placeholder,context_batch_placeholder, embedding, dropout_keep_rate)
+    U, _ = encoder(question_batch_ph,context_batch_ph, embedding, dropout_keep_rate)
     initer = tf.contrib.layers.xavier_initializer()
     LAYER_SIZE = 10
     W1 = tf.get_variable("W1", shape = [U.shape[2],LAYER_SIZE], initializer = initer, dtype = tf.float32)
@@ -36,7 +45,9 @@ def build_classifier(embedding):
     W3_batch = tf.stack([W3] * batch_size); b3_batch = tf.stack([b3] * batch_size)
     out = tf.tanh(tf.matmul(U,W1_batch)+b1_batch)
     out = tf.tanh(tf.matmul(out,W2_batch)+b2_batch)
-    out = tf.tanh(tf.matmul(out, W3_batch) + b3_batch)
-    loss = tf.nn.softmax_cross_entropy_with_logits(labels = answer_start, logits = out)
+    out = tf.tanh(tf.matmul(out, W3_batch) + b3_batch , name = "classifier_output")
+    loss = tf.nn.softmax_cross_entropy_with_logits(labels = has_answer_ph, logits = out, name = "loss_v2_classifier")
     optimizer = tf.train.AdamOptimizer(CONFIG.LEARNING_RATE)
-    train_op = optimizer.minimize(loss, name = "train_op")
+    train_op = optimizer.minimize(loss, name = "train_op_classifier")
+
+    return train_op, loss, out
